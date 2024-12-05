dbf1e8189d711fc3e770a51492117890f10cb62e
NyxVoid <amrit44404@proton.me>
2024-12-05 12:36:24 +0545

    adding hugo package

--- install.sh
+++ install.sh
@@ -16,7 +16,8 @@ EOF
 sudo xbps-install xorg base-devel harfbuzz-devel libX11-devel libXinerama-devel \
 	libXft-devel xwallpaper htop xclip xset xdotool lf alsa-utils font-awesome6 adwaita-icon-theme \
 	nerd-fonts-symbols-ttf noto-fonts-emoji slock xcompmgr fastfetch firefox nsxiv neovim mpv newsboat \
-	bleachbit unzip zathura zathura-pdf-poppler scrot xf86-video-intel NetworkManager tmux rust go ripgrep
+	bleachbit unzip zathura zathura-pdf-poppler scrot xf86-video-intel NetworkManager tmux \
+	rust go ripgrep hugo
 
 # gimp obs
 # wget nodejs
