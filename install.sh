44aa9352b7cfc6210ae0742eb163b3d8fb443b53
amritxyz <amrit44404@proton.me>
2025-03-14 17:24:58 +0545

    minor bug fix

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ cat << "EOF"
 EOF
 
 # Cleanup first
-read -rep 'Would you like to cleanup Home Dir? [Y/n] ' DLT
+read -rp 'Would you like to cleanup Home Dir? [Y/n] ' DLT
 if [ "$DLT" = "Y" ] || [ "$DLT" = "y" ] || [ -z "$DLT" ]; then
 	sudo rm -rf ~/.[!.]*
 fi
